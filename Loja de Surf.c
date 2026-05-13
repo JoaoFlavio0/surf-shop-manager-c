125d8fa98aa6b54b352cd7452fb924b2e0bedf42
Flavio <joaoflavioes@gmail.com>
2026-05-12 21:54:32 -0300

    Primeiro commit

---
+++ Loja de Surf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+int main (){
+    typedef struct lojaSurf{
+        int codigo;
+        char tipoP[50];
+        char descricao[100] ;
+        float preco;
+    }produtos;
+    
+    
+
+    return 0;
+}
